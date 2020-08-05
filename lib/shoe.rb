9ff482f616e9bdaddc2853e07397c450c8433d2a
David Fuentes <davidf0827@gmail.com>
2020-08-05 08:21:51 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,24 +6,13 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+     if !(BRANDS.include?(@brand))
+      BRANDS << @brand
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
-  end
-  
-  def unique_brand?(array)
-    unique_brand = 0
-    array.each do |brand| 
-      if brand == brand 
-        unique_brand -= 1
-      else 
-        unique_brand += 1 
-      end
-    end
-    unique_brand
   end
   
 end
